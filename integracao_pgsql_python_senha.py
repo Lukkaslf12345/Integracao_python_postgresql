cc939d7d04b0ca25ae7fa81220d93136062bc43e
Lucas_Gasparetto <143142652+Lukkaslf12345@users.noreply.github.com>
2024-08-23 15:41:16 -0300

    Create integracao_pgsql_python_senha.py
    Validação somente de senha.

---
+++ integracao_pgsql_python_senha.py
@@ -0,0 +1,26 @@
+import psycopg2
+from pyquery.query import *
+from pyquery.connection import *
+
+Connection = TConnection(user = "postgres", 
+                                  password = "senha_banco_dados",
+                                  host = "ip_banco_dados",
+                                  port = "porta_banco_dados",
+                                  database = "base_banco_dados")                                  
+Q1 = TQuery()
+Q1.connection = Connection
+
+input_login = str(input('[E]ntrar  [S]air: '))
+
+if input_login == 'E':
+    senha_digitada = str(input('Digite sua senha: '))
+    Q1.sql.text = 'select senha from senha'
+    Q1.Open()
+    senha_permitida = Q1.FieldByName('senha').AsString
+    
+    if senha_digitada == senha_permitida:
+        print('Você entrou no sistema!')
+    else:
+        print('A senha está errada!')
+else:
+    print('Você saiu do sistema!')
